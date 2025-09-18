08c7a5092db604578e4a99c81b7cb2d8c8458d82
Sanket Gaikwad <sanketlike63@gmail.com>
2025-09-18 12:26:43 +0530

    step redirectopn temp

--- src/services/authAxiosInstance.js
+++ src/services/authAxiosInstance.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const AuthAPI = axios.create({
-  baseURL: import.meta.env.VITE_AUTH_BASE_URL, // http://127.0.0.1:3000/api/v2
+  baseURL: import.meta.env.VITE_AUTH_BASE_URL,
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
